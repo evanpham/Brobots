3d001798af76e468d61f8adde3c7d4a3566d3b68
evanpham <erpham16@gmail.com>
2019-07-17 16:19:00 -0700

    Added arm and sonar class

--- LineFollower/src/main.cpp
+++ LineFollower/src/main.cpp
@@ -3,6 +3,8 @@
 #include <Adafruit_SSD1306.h>
 #include <FreeMono9pt7b.h>
 #include <time.h>
+#include <Sonar.h>
+#include <Arm.h>
 
 // #if (SSD1306_LCDHEIGHT != 64)
 // #error("Height incorrect, please fix Adafruit_SSD1306.h!");
@@ -13,25 +15,36 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 #define pot PA2
 #define button PA12
-#define motorL PA0
-#define motorR PB0
+#define motorL PA_0
+#define motorR PB_0
 #define leftQRD PB10
 #define leftestQRD PB11
 #define rightQRD PA4
 #define rightestQRD PA3
-#define trig PB14
-#define echo PB13
+#define upEcho PB12
+#define upTrig PB13
+#define outEcho PB14
+#define outTrig PB15
+#define baseServo PB_4
+#define clawServo PA_5
+#define armOut PA_6
+#define armIn PA_7
+#define armUp PA_3
+#define armDown PA_2
 
-int potVal, oldPot, correction;
+// PWM limits, right and left value, and correction value
+int correction;
 int minPWM = 0;
 int maxPWM = 400;
 int PWMleft = (maxPWM - minPWM)/2;
 int PWMright = (maxPWM - minPWM)/2;
 
+// PID gains, tuning pot, increment, and error variables
 float Kp = 65.0;
 float Kd = 65.0;
-
-int last = millis(); // Timer used to limit mode changes
+float inc = 1;
+int potVal, oldPot;
+int error, lastError, deltaError = 0;
 
 // Timers used for derivative calculations
 int lastSwitch = millis();
@@ -51,14 +64,21 @@ bool moving = false;
 bool left = false;
 bool right = false;
 
-
-int error, lastError, deltaError = 0;
-float inc = 1;
-
+// Function prototypes
 void change_mode(void);
 float calcDerivative(void);
 void updateError(void);
 int readSonar(void);
+void getStone(Arm a);
+
+// Arm and Sonar initialization
+// out and up are sonars which measure distance out and up respectively
+// All other inputs are pin names corresponding to specific control pins
+// Ex. armUp is a pin which, when powered, moves the arm up
+Sonar up(upEcho, upTrig);
+Sonar out(outEcho, outTrig); 
+Arm arm(baseServo, clawServo, armOut, armIn, armUp, armDown, out, up);
+
 
 void setup() {
   Serial.begin(9600);
@@ -66,10 +86,10 @@ void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x64)
   // init done
 
-  // Draw a test
+  // Startup display
   display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
+  // display.setTextSize(1);
+  // display.setTextColor(WHITE);
   display.setFont(&FreeMono9pt7b);
   display.setCursor(3,10);
   display.println("Edith");
@@ -87,8 +107,6 @@ void setup() {
   pinMode(rightQRD, INPUT_PULLUP);
   pinMode(leftestQRD, INPUT_PULLUP);
   pinMode(rightestQRD, INPUT_PULLUP);
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT);
   // attachInterrupt(digitalPinToInterrupt(button), change_mode, LOW);
 
 }
@@ -101,8 +119,8 @@ void loop() {
 
   if (tuneKp) {
     // Tune Kp mode (Turn off motors)
-    pwm_stop(PA_0);
-    pwm_stop(PB_0);
+    pwm_stop(motorL);
+    pwm_stop(motorR);
 
     oldPot = potVal;
     potVal = analogRead(pot);
@@ -117,10 +135,10 @@ void loop() {
     display.print(Kp);
     display.display();
 
-    } else if (tuneKd) {
+  } else if (tuneKd) {
     // Tune Kd mode (Turn motors off)
-    pwm_stop(PA_0);
-    pwm_stop(PB_0);
+    pwm_stop(motorL);
+    pwm_stop(motorR);
 
     oldPot = potVal;
     potVal = analogRead(pot);
@@ -135,22 +153,22 @@ void loop() {
     display.print(Kd);
     display.display();
 
-    } else if (freeSpins) {
+  } else if (freeSpins) {
     // Free potentiometer spins (Turn motors off)
-    pwm_stop(PA_0);
-    pwm_stop(PB_0);
+    pwm_stop(motorL);
+    pwm_stop(motorR);
 
     display.clearDisplay();
     display.setCursor(4,40);
     display.print("FREE SPINS");
     display.display();
 
-    } else {
+  } else {
     // If not moving, ramp up speed
     if (!moving) {
       for (int PWMval = 0; PWMval < PWMleft; PWMval++) {
-        pwm_start(PB_0, 100000, 500, PWMval, 1);
-        pwm_start(PA_0, 100000, 500, PWMval, 1);
+        pwm_start(motorR, 100000, 500, PWMval, 1);
+        pwm_start(motorL, 100000, 500, PWMval, 1);
         delay(2);
       }
       moving = true;
@@ -177,8 +195,8 @@ void loop() {
       PWMright = maxPWM;
     }
 
-    pwm_start(PB_0, 100000, 500, PWMright, 1);
-    pwm_start(PA_0, 100000, 500, PWMleft, 1);
+    pwm_start(motorR, 100000, 500, PWMright, 1);
+    pwm_start(motorL, 100000, 500, PWMleft, 1);
     // Serial.println(correction);
     // Serial.println(PWMleft);
     // Serial.println(PWMright);
@@ -337,14 +355,17 @@ float calcDerivative(void) {
   return derivative;
 }
 
-//shift mode by button press
-//tuneKp mode changes Kp with potentiometer
-//tuneKd mode changes Kd with potentiometer
-//freeSpins mode changes QRD threshold with potentiometer
-//default mode (both false) tries to follow tape
+// Shift mode by button press
+// tuneKp mode changes Kp with potentiometer
+// tuneKd mode changes Kd with potentiometer
+// freeSpins mode changes QRD threshold with potentiometer
+// Default mode (all others false) tries to follow tape
 void change_mode(void) {
+  // Mark moving boolean as false to trigger ramp up when motion is resumed
   moving = false;
   delay(100);
+
+  // Continue if button still pressed after delay (helps combat noise)
   if (!digitalRead(button)) {
     // move to next mode
     if (!tuneKp && !tuneKd && !freeSpins) {
@@ -368,6 +389,13 @@ void change_mode(void) {
       display.println("ZOOM!");
       display.display();
     }
-    last = millis();
   }
+}
+
+// Function for aqcuiring a stone from a pillar
+// a is the Arm object which controls all motion of the robot arm and claw
+// Motion must include outward motion to pillar, upward motion to stone, claw closure,
+// deposition maneuvering, and claw openning, and return to default position
+void getStone(Arm a) {
+
 }
